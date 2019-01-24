ad1eeb392275218401a42c9cfaac19cc40d3ce18
salvenal <vsoldatov@issart.com>
2019-01-24 16:59:02 +0600

    Optimization the project

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -37,7 +37,7 @@ public class ContactDataGenerator {
     }
 
     generator.run();
-    }
+  }
 
   private void run() throws IOException {
     List<ContactData> contacts = generateContacts(count);
@@ -52,9 +52,9 @@ public class ContactDataGenerator {
     }
   }
 
-  private  List<ContactData> generateContacts(int count) {
+  private List<ContactData> generateContacts(int count) {
     List<ContactData> contacts = new ArrayList<ContactData>();
-    for (int i = 0; i< count; i++) {
+    for (int i = 0; i < count; i++) {
       contacts.add(new ContactData().withFirstname(String.format("firstname %s", i))
               .withLastname(String.format("lastname %s", i))
               .withAddress(String.format("address %s", i)));
@@ -64,10 +64,10 @@ public class ContactDataGenerator {
     return contacts;
   }
 
-  private  void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
+  private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
     try (Writer writer = new FileWriter(file)) {
-      for (ContactData contact: contacts) {
-        writer.write(String.format("%s;%s;%s\n", contact.getFirstname(),contact.getLastname(),contact.getAddress()));
+      for (ContactData contact : contacts) {
+        writer.write(String.format("%s;%s;%s\n", contact.getFirstname(), contact.getLastname(), contact.getAddress()));
       }
     }
   }
@@ -80,12 +80,13 @@ public class ContactDataGenerator {
 
     }
   }
+
   private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
     XStream xstream = new XStream();
     xstream.processAnnotations(ContactData.class);
     String xml = xstream.toXML(contacts);
     try (Writer writer = new FileWriter(file)) {
-    writer.write(xml);
+      writer.write(xml);
     }
   }
 }
