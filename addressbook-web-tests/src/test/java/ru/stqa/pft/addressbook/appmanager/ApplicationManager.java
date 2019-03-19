abd3188a0e15025ac2e95182a0a50b43a1ddb8ef
salvenal <vsoldatov@issart.com>
2019-03-19 11:47:28 +0600

    Fixed chromedriver in addressbook-tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -41,8 +41,7 @@ public class ApplicationManager {
     if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("/home/user/Desktop/firefox/firefox"));
     } else if (browser.equals(BrowserType.CHROME)) {
-      System.setProperty("webdriver.chrome.driver","/usr/lib/chromium-browser/chromedriver");
-      wd = new ChromeDriver();
+	  wd = new ChromeDriver();
     } else if (browser.equals(BrowserType.IE)) {
       wd = new InternetExplorerDriver();
     } else if (browser.equals(BrowserType.OPERA)) {
