39e3cfe85a8bafe7e07dcc8910b90b811588a863
salvenal <vsoldatov@issart.com>
2019-03-19 10:56:37 +0600

    Fix sandbox

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -11,7 +11,7 @@ public class PointTests {
   public void testArea() {
     Point p1 = new Point(2,0);
     Point p2 = new Point(1,0);
-    Assert.assertEquals(Point.distance(p1,p2), 1);
+    Assert.assertEquals(Point.distance(p1,p2), 1.0);
   }
 
 
